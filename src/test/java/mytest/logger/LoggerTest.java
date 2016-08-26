7382e35d5e78e5232040cbddb166848cb27f2348
Nadya <db.javaschool@gmail.com>
2016-08-26 18:07:55 +0300

    Test writing training: dirty code

---
+++ src/test/java/mytest/logger/LoggerTest.java
@@ -0,0 +1,20 @@
+package mytest.logger;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class LoggerTest {
+    //region given
+    //endregion
+
+    @Test
+    public void shouldPrintTypeAndCharacterWhenCharacterLogged() {
+
+    }
+
+    @Test
+    public void shouldPrintTypeAndBooleanWhenBooleanLogged() {
+
+    }
+}
