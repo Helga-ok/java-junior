60cf905dcd82da35928869e42bf531886afe223b
Nadya <db.javaschool@gmail.com>
2016-08-29 14:51:50 +0300

    Add two unit tests

--- src/test/java/mytest/logger/LoggerTest.java
+++ src/test/java/mytest/logger/LoggerTest.java
@@ -1,20 +1,49 @@
 package mytest.logger;
 
+import com.acme.edu.*;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class LoggerTest {
     //region given
     //endregion
 
     @Test
-    public void shouldPrintTypeAndCharacterWhenCharacterLogged() {
+    public void shouldPrintTypeAndCharacterWhenCharacterLogged() throws Exception, LogException {
+        //region Given
+        Saver saverStub = mock(Saver.class);
+        Decorator decoratorStub = mock(Decorator.class);
+        Logger logger = new Logger(decoratorStub, saverStub);
+        //endregion
 
+        //region When
+        logger.log('a');
+        //endregion
+
+        //region Then
+        verify(saverStub).save("char: a");
+        //endregion
     }
 
     @Test
-    public void shouldPrintTypeAndBooleanWhenBooleanLogged() {
+    public void shouldPrintTypeAndBooleanWhenBooleanLogged() throws LogException, SaveException, DecorateException {
+        //region Given
+        Saver saverStub = mock(Saver.class);
+        Decorator decoratorStub = mock(Decorator.class);
+        when(decoratorStub.decorate("primitive: true")).thenReturn("primitive: true");
+        Logger logger = new Logger(decoratorStub, saverStub);
+        //endregion
 
+        //region When
+        logger.log(true);
+        //endregion
+
+        //region Then
+        verify(decoratorStub).decorate("primitive: true");
+        verify(saverStub).save("primitive: true");
+        //endregion
     }
 }
